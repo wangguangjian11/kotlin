bc76591f40e59d37bd69c4d9f8893391e15fb5db
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-11-13 12:42:47 +0100

    Fix compilation of GenerateTests

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1054,9 +1054,16 @@ fun main(args: Array<String>) {
         }
     }
 
+
+    /*
+
+
     testGroup("plugins/android-extensions/android-extensions-jps/test", "plugins/android-extensions/android-extensions-jps/testData") {
         testClass<AbstractAndroidJpsTestCase> {
             model("android", recursive = false, extension = null)
         }
     }
+
+    
+    */
 }
